f58510c0a41ebb73e516eb0e41f1f48e982e54cf
anugnes <nugnes.alberto@gmail.com>
2017-12-04 18:00:31 +0000

    Add variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,16 +1,17 @@
-variable "name" {}
+variable "name" {
+  description = "VPC name visible from the AWS console"
+}
 
-variable "vpc_cidr" {}
-variable "public_subnets" {}
-variable "private_subnets" {}
+variable "vpc_cidr" {
+  description = "CIDR block to use for the VPC"
+}
+variable "public_subnets" {
+  description = "Comma separated list of CIDR blocks for the public subnets (1 subnet per availability zone)"
+}
+variable "private_subnets" {
+  description = "Comma separated list of CIDR blocks for the private subnets (1 subnet per availability zone)"
+}
 
-variable "azs" {}
-//variable "region" {}
-//variable "route_zone_id" {}
-//variable "domain" {}
-
-//variable "key_name" {}
-//variable "bastion_instance_type" {}
-//variable "openvpn_instance_type" {}
-//variable "ebs_region" {}
-//variable "ebs_size" {}
+variable "azs" {
+  description = "Comma separated list of availability zones to include in the VPC"
+}
